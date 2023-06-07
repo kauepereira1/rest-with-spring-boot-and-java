02fbce81b1c4c93c74f2b74d0876ab0efe499d6e
Mariana <mariana.vilareal@hotmail.com>
2023-06-07 15:50:02 -0300

    Primeira etapa em Java com Spring  Boot 3

---
+++ 05_FirstSetupInJavawithSpringBoot/rest-with-spring-boot-and-java-1/src/main/java/br/com/kaue/Startup.java
@@ -0,0 +1,13 @@
+package br.com.kaue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
