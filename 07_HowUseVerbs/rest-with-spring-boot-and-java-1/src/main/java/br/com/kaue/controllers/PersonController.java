49139e9697ebb58e3dbb78d5e0e229290de94c75
Mariana <mariana.vilareal@hotmail.com>
2023-06-08 02:48:27 -0300

    Alteracao

---
+++ 07_HowUseVerbs/rest-with-spring-boot-and-java-1/src/main/java/br/com/kaue/controllers/PersonController.java
@@ -0,0 +1,34 @@
+package br.com.kaue.controllers;
+
+import br.com.kaue.exceptions.UnsuportedMathOperationException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class PersonController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    //Endpoint da API e seus parametros obrigatórios
+    @RequestMapping(value = "/soma/{numeroUm}/{numeroDois}",
+            //Metodo da API
+            method = RequestMethod.GET)
+    public Double soma(
+            //Recupera dados da url
+            @PathVariable(value = "numeroUm") String numeroUm,
+            @PathVariable(value = "numeroDois") String numeroDois
+        //Lançando uma exception
+        ) throws Exception{
+
+        // Validando se a String é numerica
+        if(!isNumeric(numeroUm) || !isNumeric(numeroDois)) {
+            throw new UnsuportedMathOperationException("Please set a numeric value");
+        }
+
+        //Convertendo e somando os dois numero
+        return math.soma(convertToDouble(numeroUm), convertToDouble(numeroDois));
+    }
+
+}
---
+++ 07_HowUseVerbs/rest-with-spring-boot-and-java-1/src/main/java/br/com/kaue/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.com.kaue.exceptions.handler;
+
+import br.com.kaue.exceptions.ExceptionResponse;
+import br.com.kaue.exceptions.UnsuportedMathOperationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handlerAllExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsuportedMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handlerBadRequestExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
